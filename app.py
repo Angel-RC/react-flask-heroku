842bc2d0768c22d7a71c3bab8094690fc4fb3714
Ángel RC <37696271+Angel-RC@users.noreply.github.com>
2021-02-13 10:20:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 
-app = Flask(__name__, static_folder='build/', static_url_path='/')
+app = Flask(__name__, static_folder='public/', static_url_path='/')
 app.debug = 'DEBUG' in os.environ
 
 
